accc2bda9c7924898af2fb157f3a2dafe4b9d154
koher <koher@koherent.org>
2015-09-25 15:22:02 +0900

    Update the README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# PromiseKotlin
+PromiseKotlin
+==========================
+
+_PromiseKotlin_ is a Promise library which has __similar APIs to [_PromiseK_](https://github.com/koher/PromiseK)__.
+
+```kotlin
+// `flatMap` is equivalent to `then` of JavaScript's `Promise`
+val a: Promise<Int> = asyncGet(2).flatMap { asyncGet(it) }.flatMap { asyncGet(it) }
+val b: Promise<Int> = asyncGet(3).map { it * it }
+val sum: Promise<Int> = a.flatMap { a0 -> b.flatMap { b0 -> Promise(a0 + b0) } }
+
+// uses Nullable for error handling
+val mightFail: Promise<Int?> = asyncFailable(5).flatMap { Promise(it?.let { it * it }) }
+val howToCatch: Promise<Int> = asyncFailable(7).flatMap { Promise(it ?: 0) }
+
+// a failable operation chain with `flatMap2`
+val failableChain: Promise<Int?> = asyncFailable(11).flatMap2 { it?.let { asyncFailable(it) } }
+```
+
+License
+--------------------------
+
+[The MIT License](LICENSE)
--- project/app/src/androidTest/java/org/koherent/promisekotlin/PromiseTest.kt
+++ project/app/src/androidTest/java/org/koherent/promisekotlin/PromiseTest.kt
@@ -151,6 +151,20 @@ public class PromiseTest : TestCase() {
             fail(e.getMessage())
         }
     }
+
+    public fun testSample() {
+        // `flatMap` is equivalent to `then` of JavaScript's `Promise`
+        val a: Promise<Int> = asyncGet(2).flatMap { asyncGet(it) }.flatMap { asyncGet(it) }
+        val b: Promise<Int> = asyncGet(3).map { it * it }
+        val sum: Promise<Int> = a.flatMap { a0 -> b.flatMap { b0 -> Promise(a0 + b0) } }
+
+        // uses Nullable for error handling
+        val mightFail: Promise<Int?> = asyncFailable(5).flatMap { Promise(it?.let { it * it }) }
+        val howToCatch: Promise<Int> = asyncFailable(7).flatMap { Promise(it ?: 0) }
+
+        // a failable operation chain with `flatMap2`
+        val failableChain: Promise<Int?> = asyncFailable(11).flatMap2 { it?.let { asyncFailable(it) } }
+    }
 }
 
 fun asyncGet(value: Int): Promise<Int> {
@@ -167,6 +181,16 @@ fun asyncGetOrFail(value: Int, fails: Boolean): Promise<Int?> {
         Promise<Int?>(null)
     } else {
         asyncGet(value).map {(x: Int): Int? -> x }
+    }
+}
+
+fun asyncFailable(value: Int): Promise<Int?> {
+    return asyncGet(value).map {
+        if (Math.random() < 0.5) {
+            it
+        } else {
+            null
+        }
     }
 }
 
